14873386ae5e3cf9ff0f5b9edbb7dd29aee50f4c
Samuteg <samuneveslopes@gmail.com>
2026-03-08 21:17:18 -0300

    all ajust to buy a domain

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,7 +1,7 @@
 const DEFAULT_API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://tasknest-1-nrsr.onrender.com";
+    : "https://tasknest-otql.onrender.com";
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || DEFAULT_API_URL;
 
