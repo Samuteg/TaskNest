924f5f0e656ede1f14b38c00064884a3fe79751e
Samuteg <151961384+Samuteg@users.noreply.github.com>
2026-04-29 03:09:15 +0000

    change to vercel backend

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,5 +1,7 @@
 const isDev = process.env.NODE_ENV === "development";
-const defaultApiUrl = isDev ? "http://localhost:5000" : "";
+const defaultApiUrl = isDev
+  ? "http://localhost:5000"
+  : "https://task-nest-backend-dusky.vercel.app";
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? defaultApiUrl;
 
